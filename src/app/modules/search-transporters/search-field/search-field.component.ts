a7de0f93cd76f80e49f236869edca6486ff56f84
Vladyslav Iaresko <vladyaresko01@gmail.com>
2023-07-25 18:08:55 +0300

    fixes

--- src/app/modules/search-transporters/search-field/search-field.component.ts
+++ src/app/modules/search-transporters/search-field/search-field.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Transporter } from 'src/app/shared/models/transporter.model';
 @Component({
@@ -13,7 +19,6 @@ export class SearchFieldComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
     this.cargoForm = this.createCargoForm();
   }
-
   ngOnInit() {}
 
   createCargoForm() {
@@ -37,8 +42,19 @@ export class SearchFieldComponent implements OnInit {
   onSubmit() {
     if (this.cargoForm.valid) {
       this.searchCargo.emit(this.cargoForm.value as Transporter);
+      this.cargoForm.markAsPristine();
       this.cargoForm.reset();
+      this.resetFormControls(this.cargoForm);
     } else {
     }
+  }
+  private resetFormControls(formGroup: FormGroup): void {
+    Object.values(formGroup.controls).forEach((control) => {
+      this.cargoForm.markAsPristine();
+      this.cargoForm.updateValueAndValidity();
+      if (control instanceof FormGroup) {
+        this.resetFormControls(control);
+      }
+    });
   }
 }
